8dd255beaad3d1b7dafb8dfdc1c0e258cba97160
Pedro Cruz <54217967+Hyakutak@users.noreply.github.com>
2023-04-19 08:50:35 -0300

    adjustments in profile state

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../../../lib/axios';
+
 export function Profile() {
+    const [profile, setProfile] = useState({});
+    const username = 'hyakutak';
+
+    async function fetchProfile() {
+        const response = await api.get(`users/${username}`);
+        setProfile(response.data);
+    }
+
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+
     return (
         <span>oiiii</span>
     );
